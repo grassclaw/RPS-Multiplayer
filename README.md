1b9918a14fae4eabcb99957092d654f86a814cdc
shadowlaq <escamillaa@email.arizona.edu>
2018-06-15 00:11:36 -0700

    reformat page

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
-# RPS-Multiplayer
-Rock Paper Scissors
-
-
-This is for two players. There will be a waiting line for anyone trying to hop on. Only two players at a time right now. Sorry!
+# RPS-V2
+Version 2 of Rock paper scissors
--- assets/logic.js
+++ assets/logic.js
@@ -1,4 +1,3 @@
-$(document).ready(function () {
 
     // Initialize Firebase
     var config = {
@@ -9,253 +8,262 @@ $(document).ready(function () {
         storageBucket: "rock-paper-scissors-af009.appspot.com",
         messagingSenderId: "433955021403"
     };
-    // firebase.initializeApp(config);
+    firebase.initializeApp(config);
 
-    var app = firebase.initializeApp(config);
+var fire = firebase.database();
+var data;
 
-    // Alias database and sub-levels.
-    var database = firebase.database();
-    var chats = database.ref('chat');
-    var connections = database.ref('connections');
-
-    // Initialize player/opponent data.
-    var con;
-    var player = {
-        number: '0',
-        name: '',
-        wins: 0,
-        losses: 0,
-        turns: 0,
-        choice: ''
-    };
-    var opponent = {
-        number: '0',
-        name: '',
-        wins: 0,
-        losses: 0,
-        turns: 0,
-        choice: ''
-    };
-    var waiting = false;
-
-    // jQuery selectors used in multiple places.
-    var messages = $('.messages');
-    var username = $('#username');
-
-    // Initial connection to Firebase/presence handling.
-    // This is 'once' and not 'on'.
-    connections.once('value', function (snapshot) {
-        if (Object.keys(snapshot.val()).indexOf('1') === -1) {
-            player.number = '1';
-            opponent.number = '2';
-        } else if (Object.keys(snapshot.val()).indexOf('2') === -1) {
-            player.number = '2';
-            opponent.number = '1';
-        }
-
-        // If you got a player number, you're 1 or 2.
-        if (player.number !== '0') {
-            // Make a connection to Firebase and send your info.
-            con = connections.child(player.number);
-            con.set(player);
-
-            // When I disconnect, remove this device.
-            con.onDisconnect().remove();
-
-            // If 1 and 2 were taken, your number is still 0.
-        } else {
-            // Remove the name form and put the alert there.
-            $('section').remove();
-            $('.alert').show();
-            // And disconnect from Firebase.
-            app.delete();
-        }
-    });
+var playObj = {
+	player1: {
+		name: 1,
+		play: 1
+	},
+	player2: {
+		name: 1,
+		play: 1
+	},
+	plays: {
+		myPlay: 1,
+		opponentPlay: 1
+	},
+	wins: {
+		myWins: 0,
+		opponentWins: 0,
+		ties: 0
+	},
+	opponentName: null
+}
 
 
-    // Ongoing event listening.
-    connections.on('value', function (snapshot) {
-        // If the player is connected,
-        if (con) {
-            // And an opponent is connected,
-            if (Object.keys(snapshot.val()).indexOf(opponent.number) !== -1) {
-                // Gather the latest info about your opponent and also yourself.
-                opponent = snapshot.val()[opponent.number];
-                player = snapshot.val()[player.number];
-                // If we have a name for our opponent,
-                if (opponent.name.length > 0) {
-                    // Show the opponent. This also updates the opponents info over time.
-                    DOMFunctions.showOpponentInfo();
-                    // Once both players have a name,
-                    if (player.name.length > 0) {
-                        // Check each time whether the players have made selections.
-                        var choice1 = snapshot.val()['1'].choice;
-                        var choice2 = snapshot.val()['2'].choice;
-                        var turns1 = snapshot.val()['1'].turns;
+function start() {
+	fire.ref().set({
+			player1: {
+				name: 1,
+				play: 1
+			},
+			player2: {
+				name: 1,
+				play: 1
+			}
+		}); // end ref set
+}
 
-                        // If both have picked, run getWinner on those choices.
-                        if (choice1.length > 0 && choice2.length > 0) {
-                            getWinner(choice1, choice2);
-                            // If player 1 hasn't chosen yet, show them their options.
-                        } else if (choice1.length === 0 && turns1 === 0) {
-                            DOMFunctions.showMoveOptions('1');
-                            // Otherwise player 2 must be the one who hasn't make a choice yet.
-                        } else if (choice1.length > 0 && choice2.length === 0) {
-                            DOMFunctions.showMoveOptions('2');
-                        }
-                    }
-                }
-            } else if (opponent.name.length > 0 && Object.keys(snapshot.val()).indexOf(opponent.number) === -1) {
-                $('.turn').text('Opponent left. Waiting for new opponent.');
-                $('.waiting-' + opponent.number).show();
-                $('.name-' + opponent.number).empty();
-                $('.win-loss-' + opponent.number).empty();
-            }
-        }
-    });
+function giveName() {
+	$('.row-start').slideUp(2000);
+	console.log('initial value of firebase name1 is ' + data.player1.name);
+
+	if((data.player1.name === 1) && ($('#name-input').val() !== '')) {
+		playObj.player1.name = $('#name-input').val();
+		console.log('name input in player1.name block is ' + $('#name-input').val());
+		console.log('local name 1 is ' + playObj.player1.name);
+		fire.ref().update({
+			player1: {
+				name: playObj.player1.name,
+				play: 1
+			}
+		}); // end firebase set data.player1.name
+		playObj.player1.name = data.player1.name;
+		$('#name-input').val('');
+		$('.row-message').show();
+		$('.col-message').html("<span>Waiting for an opponent...</span>");
+		console.log('firebase player1.name is ' + data.player1.name);
+		player2OpponentSet();
+	} else if(($('#name-input').val() !== '') && (data.player1.name != playObj.player1.name) && (data.player2.name === 1)) {
+		playObj.player2.name = $('#name-input').val();
+		console.log('name input in name2 block is ' + $('#name-input').val());
+		console.log('local player2.name is ' + playObj.player2.name);
+		fire.ref().update({
+			player2: {
+				name: playObj.player2.name,
+				play: 1
+			}
+		}); // end firebase set name2
+		playObj.player2.name = data.player2.name;
+		$('#name-input').val('');
+		console.log('firebase name 2 is ' + data.player2.name);
+		playObj.opponentName = data.player1.name;
+		console.log('local opponent name (1) is ' + playObj.opponentName);
+		playersTogether();
+
+	} // set player1 and player2
+
+} // end giveName
+
+function player2OpponentSet() {
+	if((data.player2.name !== playObj.player2.name) && (data.player1.name === playObj.player1.name) && (data.player2.name !== 1)) {
+		playObj.player2.name = data.player2.name;
+		playObj.opponentName = data.player2.name;
+		console.log('local opponent name (2) is ' + playObj.opponentName);
+		playersTogether();
+	} else {
+		setTimeout(player2OpponentSet, 500);
+	}
+} // player2OpponentSet
+
+function playersTogether() {
+	if(playObj.opponentName !== null) {
+		console.log('opponent is ' + playObj.opponentName);
+		$('.row-message').show();
+		$('.col-message').html("<span>You're playing with " + playObj.opponentName + " today.  Click rock, paper or scissors to play a round.  Have fun!</span>");
+		setTimeout(startPlay, 2000);
+	} // show begin message
+} // end playersTogether
+
+function startPlay() {
+	$('.row-message').slideUp();
+	$('.row-play-choice').slideDown();
+} // end startPlay
+
+function nameFocus(){
+	$('#name-input').focus(function() {
+		if((($('#name-input').attr('value')) || ($('#name-input').val()))  == 'Name') {
+			$('#name-input').attr('value', '');
+			$('#name-input').val('');
+			console.log('onfocus ' + $('#name-input').attr('value'), $('#name-input').val());
+		} // name-input focus if
+	}); // name-input focus
+	$('#name-input').blur(function() {
+		if((($('#name-input').attr('value')) || ($('#name-input').val()))  == '') {
+			$('#name-input').attr('value', 'Name');
+			$('#name-input').val('Name');
+			console.log('onblur ' + $('#name-input').attr('value'), $('#name-input').val());
+		} //name-input blur if
+	}); // name-input blur
+} //end nameFocus
+
+function makeMove(event) {
+		playObj.plays.myPlay = $(event.target).data('move');
+		console.log('myPlay is ' + playObj.plays.myPlay);
+		$('.row-play-choice').hide();
+		$('.row-play-battle').show();
+		$('.pic-my-play').html('<img class="battle-img" src="img/' + playObj.plays.myPlay + '.jpg" alt="You played ' + playObj.plays.myPlay + '" />');
+		$('.my-move-caption').html('<span>' + playObj.plays.myPlay + '</span>');
+		if((playObj.opponentName === data.player2.name) && (data.player1.play === 1)) {
+			console.log('player 1 set play value in firebase');
+			playObj.player1.play = playObj.plays.myPlay;
+			fire.ref().update({
+				player1: {
+					name: data.player1.name,
+					play: playObj.player1.play
+				}
+			}); // end firebase update data.player1.play
+			console.log('set player1 play complete.  playObj.plays.myPlay for player 1 is "' + playObj.plays.myPlay + '" and data.player1.play is "' + data.player1.play +'"');
+			setLocalOpponent1Play();
+		} else if((playObj.opponentName === data.player1.name) && (data.player2.play === 1)){
+			console.log('player2 set play value in firebase');
+			playObj.player2.play = playObj.plays.myPlay;
+			fire.ref().update({
+				player2: {
+					name: data.player2.name,
+					play: playObj.player2.play
+				}
+			}); // end firebase update data.player2.play
+			console.log('set player2 play complete.  playObj.plays.myPlay for player2 is "' + playObj.plays.myPlay + '" and data.player2.play is "' + data.player2.play +'"');
+			setLocalOpponent2Play();
+		} // end set play db values
+} // end makeMove
+function setLocalOpponent1Play() {
+		if(data.player2.play !== 1) {
+			console.log('player1 set opponent play value locally');
+			playObj.player2.play = data.player2.play;
+			playObj.plays.opponentPlay = playObj.player2.play;
+			$('.pic-opponent-play').html('<img class="battle-img" src="img/' + playObj.plays.opponentPlay + '.jpg" alt="Your opponent played ' + playObj.plays.opponentPlay + '" />');
+			$('.opponent-move-caption').html('<span>' + playObj.plays.opponentPlay + '</span>');
+			setTimeout(reckoning, 3000);
+		} else {
+			setTimeout(setLocalOpponent1Play, 500);
+		}
+} // end set local opponent 1 play
+function setLocalOpponent2Play() {
+		if(data.player1.play !== 1) {
+			console.log('player2 set opponent play value locally');
+			playObj.player1.play = data.player1.play;
+			playObj.plays.opponentPlay = playObj.player1.play;
+			$('.pic-opponent-play').html('<img class="battle-img" src="img/' + playObj.plays.opponentPlay + '.jpg" alt="Your opponent played ' + playObj.plays.opponentPlay + '" />');
+			$('.opponent-move-caption').html('<span>' + playObj.plays.opponentPlay + '</span>');
+			setTimeout(reckoning, 3000);
+		} else {
+			setTimeout(setLocalOpponent2Play, 500);
+		}// end else if set opponent values
+} // end set local opponent values
+
+	// click handler fires in first page, hide row-play-choice run set playObj.playerX.play and battle screen for nameX, push firebase playerX.play
+
+	// click handler fires in second page, hide row-play-choice run set playObj.playerX.play and battle screen for nameX, push firebase playerX.play
+
+	// if play isn't null for both players, set local playObj.playerX.play variables equal to firebase move variables and complete battle screens and reckon
+
+	// one second timeout, then animate reckoning function
+
+	// display score at the top, hide battle screen, show row-play-choice, repeat click handler
+
+function reckoning() {
+	console.log('reckoning fired');
+	$('.row-play-battle').hide();
+	$('.row-reckoning').show();
+	if(playObj.plays.opponentPlay === playObj.plays.myPlay){
+		$('.winning-pic').html('<img class="battle-img" src="img/' + playObj.plays.myPlay + '.jpg" alt="You tie!" />');
+		$('.outcome-message').html('<span>you tie!</span>');
+		playObj.wins.ties++;
+		$('.col-win').find('span').text('wins: ' + playObj.wins.myWins);
+		$('.col-tie').find('span').text('ties: ' + playObj.wins.ties);
+		$('.col-lose').find('span').text('losses: ' + playObj.wins.opponentWins);
+		console.log('ties is ' + playObj.wins.ties);
+	} else if((playObj.plays.myPlay == 'rock' && playObj.plays.opponentPlay == 'scissors') || (playObj.plays.myPlay == 'scissors' && playObj.plays.opponentPlay == 'paper') || (playObj.plays.myPlay == 'paper' && playObj.plays.opponentPlay == 'rock')){
+			$('.winning-pic').html('<img class="battle-img" src="img/' + playObj.plays.myPlay + '.jpg" alt="You win!" />');
+			$('.outcome-message').html('<span>' + playObj.plays.myPlay + ' beats ' + playObj.plays.opponentPlay + '.  you win!</span>');
+			playObj.wins.myWins++;
+			$('.col-win').find('span').text('wins: ' + playObj.wins.myWins);
+			$('.col-tie').find('span').text('ties: ' + playObj.wins.ties);
+			$('.col-lose').find('span').text('losses: ' + playObj.wins.opponentWins);
+			console.log('myWins is ' + playObj.wins.myWins);
+	} else {
+			$('.winning-pic').html('<img class="battle-img" src="img/' + playObj.plays.opponentPlay + '.jpg" alt="You lose!" />');
+			$('.outcome-message').html('<span>' + playObj.plays.opponentPlay + ' beats ' + playObj.plays.myPlay + '.  you lose!</span>');
+			playObj.wins.opponentWins++;
+			$('.col-win').find('span').text('wins: ' + playObj.wins.myWins);
+			$('.col-tie').find('span').text('ties: ' + playObj.wins.ties);
+			$('.col-lose').find('span').text('losses: ' + playObj.wins.opponentWins);
+			console.log('opponentWins is ' + playObj.wins.opponentWins);
+	} // end reckoning game logic
+	setTimeout(setNext, 3000);
+} // end reckoning
+function setNext() {
+
+	fire.ref().update({
+		player1: {
+			name: data.player1.name,
+			play: 1
+		},
+		player2: {
+			name: data.player2.name,
+			play: 1
+		}
+	});
+
+	$('.row-reckoning').hide();
+	$('.pic-opponent-play, .opponent-move-caption, .my-move-caption, .pic-my-play, .winning-pic, .outcome-message').empty();
+	$('.row-play-choice').show();
+}
 
 
-    // On-click function for submitting a name.
-    $('#submit-name').on('click', function () {
-        player.name = username.val();
-        if (player.name.length > 0) {
-            con.update({
-                name: player.name
-            });
-            DOMFunctions.showSelfJoin();
-        }
 
-        return false;
-    });
 
-    // Functions for changing HTML elements.
-    var DOMFunctions = {
-        showSelfJoin: function () {
-            username.val('');
-            $('.user-form').hide();
-            $('.waiting-' + player.number).hide();
-            $('.name-' + player.number).text(player.name);
-            $('.win-loss-' + player.number).text('Wins: ' + player.wins + ' | Losses: ' + player.losses);
-            $('.hello').text('Hello ' + player.name + '! You are player ' + player.number + '.').show();
-            $('.turn').show();
-            $('.chat-row').show();
-            $('.moves-' + opponent.number).remove();
-            this.updateScroll();
-        },
-        showOpponentInfo: function () {
-            $('.waiting-' + opponent.number).hide();
-            $('.name-' + opponent.number).text(opponent.name);
-            $('.win-loss-' + opponent.number).text('Wins: ' + opponent.wins + ' | Losses: ' + opponent.losses);
-        },
-        updatePlayerStats: function () {
-            $('.win-loss-' + player.number).text('Wins: ' + player.wins + ' | Losses: ' + player.losses);
-        },
-        updateScroll: function () {
-            messages[0].scrollTop = messages[0].scrollHeight;
-        },
-        showMoveOptions: function (currentPlayer) {
-            if (currentPlayer === player.number) {
-                $('.moves-' + currentPlayer).css('display', 'flex');
-            }
-            $('.turn').text('Player ' + currentPlayer + '\'s turn.');
-        },
-        showChats: function (snap) {
-            var chatMessage = snap.val();
-            // Only show messages sent in the last half hour. A simple workaround for not having a ton of chat history.
-            if (Date.now() - chatMessage.timestamp < 1800000) {
-                var messageDiv = $('<div class="message">');
-                messageDiv.html('<span class="sender">' + chatMessage.sender + '</span>: ' + chatMessage.message);
-                messages.append(messageDiv);
-            }
-            DOMFunctions.updateScroll();
-        },
-        showGameResult: function (message) {
-            this.updatePlayerStats();
-            $('.choice-' + opponent.number).text(opponent.choiceText).show();
-            $('.turn').hide();
-            $('.winner').text(message);
-            $('.moves').hide();
-            setTimeout(function () {
-                $('.winner').empty();
-                $('.turn').show();
-                $('.choice').empty().hide();
-                DOMFunctions.showMoveOptions('1');
-            }, 3000)
-        }
-    };
+$(document).ready(function(){
+	fire.ref().on('value', function(snapshot) {
+		data = snapshot.val();
+	});
+	start();
+	nameFocus();
+	$('#name-submit').click(giveName);
+	$(document).keypress(function(event) {
+		if(event.which === 13) {
+			giveName();
+		}
+	});
+	$('.col-play-pic').click(function(event){
+		makeMove(event)
+	});
 
-    // On-click function for selecting a move.
-    $('.move').on('click', function () {
-        var choice = $(this).data('choice');
-        var move = $(this).data('text');
-        con.update({
-            choice: choice,
-            choiceText: move
-        });
 
-        $('.moves-' + player.number).hide();
-        $('.choice-' + player.number).text(move).show();
-    });
-
-    // On-click function for submitting a chat.
-    $('#submit-chat').on('click', function () {
-        var message = $('#message');
-        var chatObj = {
-            message: message.val(),
-            timestamp: firebase.database.ServerValue.TIMESTAMP,
-            sender: player.name
-        };
-        chats.push(chatObj);
-
-        // Clear message input.
-        message.val('');
-
-        return false;
-    });
-
-    // Database listening function for chats.
-    chats.on('child_added', function (snapshot) {
-        if (snapshot.val()) {
-            DOMFunctions.showChats(snapshot);
-        }
-    });
-
-    // Win-Loss-Draw logic.
-    var getWinner = function (move1, move2) {
-        if (move1 === move2) { recordWin(); }
-        if (move1 === 'r' && move2 === 's') { recordWin('1', '2'); }
-        if (move1 === 'r' && move2 === 'p') { recordWin('2', '1'); }
-        if (move1 === 'p' && move2 === 'r') { recordWin('1', '2'); }
-        if (move1 === 'p' && move2 === 's') { recordWin('2', '1'); }
-        if (move1 === 's' && move2 === 'p') { recordWin('1', '2'); }
-        if (move1 === 's' && move2 === 'r') { recordWin('2', '1'); }
-    };
-
-    var recordWin = function (winner, loser) {
-        player.turns++;
-        connections.child(player.number).update({
-            choice: '',
-            turns: player.turns
-        });
-        // If there was a winner,
-        if (winner) {
-            // Then update your own win/loss count.
-            if (winner === player.number) {
-                player.wins++;
-                connections.child(winner).update({
-                    wins: player.wins
-                });
-            } else {
-                player.losses++;
-                connections.child(loser).update({
-                    losses: player.losses
-                });
-            }
-            // Then show the win.
-            DOMFunctions.showGameResult('Player ' + winner + ' wins!');
-        } else {
-            // Else, show the draw.
-            DOMFunctions.showGameResult('Draw.');
-        }
-    }
-});
+}); // end doc ready
